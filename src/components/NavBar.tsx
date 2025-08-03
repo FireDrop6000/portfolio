df2028424f572d2b851e7508fb8260930341982b
pratik <pratik.neel02@gmail.com>
2025-08-03 19:51:24 +0530

    bugs

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import Logo from "../icons/Logo";
 
 const NavBar = () => {
@@ -6,20 +7,20 @@ const NavBar = () => {
 			<div className="px-4">
 				<div className="flex items-center justify-between">
 					<div className="flex shrink-0">
-						<a aria-current="page" className="flex items-center" href="/">
+						<Link aria-current="page" className="flex items-center" to="/">
 							<div className="rounded-sm p-1 bg-white hover:bg-slate-100 dark:hover:bg-slate-700 hover:shadow focus:outline-none focus:ring focus:ring-slate-500/50 focus-visible:outline-none focus-visible:ring focus-visible:ring-slate-500/50 relative before:absolute before:inset-0 before:rounded-[inherit] before:bg-[linear-gradient(45deg,transparent_25%,theme(colors.white)_50%,transparent_75%,transparent_100%)] dark:before:bg-[linear-gradient(45deg,transparent_25%,theme(colors.white/.5)_50%,transparent_75%,transparent_100%)] before:bg-[length:250%_250%,100%_100%] before:bg-[position:200%_0,0_0] before:bg-no-repeat before:[transition:background-position_0s_ease] hover:before:bg-[position:-100%_0,0_0] hover:before:duration-[850ms] hover:scale-120 transition">
 								<Logo />
 							</div>
-						</a>
+						</Link>
 					</div>
 					<div className="hidden md:flex md:items-center md:justify-center md:gap-3 md:ml-13">
-						<a
+						<Link
 							aria-current="page"
 							className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
-							href="/projects"
+							to="/projects"
 						>
 							Projects
-						</a>
+						</Link>
 						<a
 							className="inline-block rounded-lg px-2 py-1 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-100 hover:text-gray-900"
 							href="#"
