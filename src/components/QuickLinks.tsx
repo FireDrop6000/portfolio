cd8e6e35fb6f3217a842148ddd9d7309633bed71
pratik <pratik.neel02@gmail.com>
2025-08-03 20:16:02 +0530

    bugs

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -4,6 +4,7 @@ const QuickLinks = () => {
 	return (
 		<div className="flex items-center justify-center md:hidden gap-6 mt-40 text-l">
 			<Link to="/projects">Projects</Link>
+			<div className="bg-blue-500 w-1 h-1 rounded-full"></div>
 			<span>Download CV</span>
 		</div>
 	);
