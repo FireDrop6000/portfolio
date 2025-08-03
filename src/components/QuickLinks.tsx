f04206815ed7230d00cb3daed3be2536499d1605
pratik <pratik.neel02@gmail.com>
2025-08-03 20:09:56 +0530

    bugs

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router";
+
 const QuickLinks = () => {
 	return (
 		<div className="flex items-center justify-center md:hidden gap-6 mt-40 text-l">
-			<a href="/projects">Projects</a>
+			<Link to="/projects">Projects</Link>
 			<span>Download CV</span>
 		</div>
 	);
