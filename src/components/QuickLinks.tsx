30b9607f7d7912c59c113d640db63664288dbef2
pratik <pratik.neel02@gmail.com>
2025-08-04 15:00:31 +0530

    view sizing

--- src/components/QuickLinks.tsx
+++ src/components/QuickLinks.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router";
 
 const QuickLinks = () => {
 	return (
-		<div className="flex items-center justify-center md:hidden gap-6 mt-40 text-l">
+		<div className="flex items-center justify-center md:hidden gap-6 mt-5 text-l">
 			<Link to="/projects">Projects</Link>
 			<div className="bg-blue-500 w-1 h-1 rounded-full"></div>
 			<span>Download CV</span>
